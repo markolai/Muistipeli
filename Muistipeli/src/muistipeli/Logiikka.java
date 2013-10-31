7d8e3d1dd65a4813c101869bf38ff9d00bd61158
Marko Laine <kheevo@gmail.com>
2013-10-31 19:07:28 +0200

    Turhia väsäyksiä, seuraavaksi sitten homma alkuun logiikkaa miettimällä

---
+++ Muistipeli/src/muistipeli/Logiikka.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muistipeli;
+
+/**
+ *
+ * @author markolai
+ */
+public class Logiikka {
+
+    public Logiikka() {
+    }
+    
+}
