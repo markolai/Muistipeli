ba815acc0934e96b8e469b82c6833d8641749d36
Marko Laine <kheevo@gmail.com>
2013-11-14 18:37:07 +0200

    Pelilaudan ja korttien luontia kehitetty ja testattu, luokkakaavio päivitetty

--- Muistipeli/src/Logiikka/Pelilauta.java
+++ Muistipeli/src/Logiikka/Pelilauta.java
@@ -24,21 +24,53 @@ public class Pelilauta {
             luoPelilauta(7);
         }
     }
-            //KESKEN
-    public void luoPelilauta(int s) {
+    //KESKEN
 
-        int[][] pelilauta = new int[4][s];
+    /**
+     *
+     * Luodaan pelilauta, parametrina vaikeusaste int v.
+     */
+    public void luoPelilauta(int v) {
+
+        int[][] pelilauta = new int[4][v];
         int k = 0;
 
-        for (int i = 0; i < s; i++) {
+        for (int i = 0; i < v; i++) {
             for (int j = 0; j < 4; j++) {
                 pelilauta[k][i] = 0;
                 j++;
             }
+
+
+        }
+        
+        luoKortit(pelilauta, v);
+        
+        tulostaPelilauta(pelilauta, v);
+        
+        
+    }
+    
+   //KESKEN
+
+    public void tulostaPelilauta(int[][] pelilauta, int v) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < v; j++) {
+                System.out.printf(" ", pelilauta[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private void luoKortit(int[][] pelilauta, int v) {
+        
+        for (int i = 0; i < v/2; i++) {
             
-            //KESKEN
+            Kortti kortti = new Kortti(i);
             
+            // sijoitetaan kortit taulukkoon ja sekoitetaan.
             
         }
+       
     }
 }
